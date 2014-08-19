a4940300bfee93d35bc2205a473a481a3d87d573
Patrick Reynolds <patrick.reynolds@kitware.com>
2014-08-19 15:48:47 -0400

    Try different approach to static.

--- icebucket.py
+++ icebucket.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__,
-            static_url_path='',
+            static_url_path='/',
             static_folder=os.path.dirname(os.path.realpath(__file__)))
 
 
@@ -10,29 +10,3 @@ app = Flask(__name__,
 def data():
     with open('icebucket.json', 'r') as json_file:
         return json_file.read()
-
-
-@app.route('/')
-def root():
-    with open('index.html', 'r') as index_file:
-        return index_file.read()
-
-
-@app.route('/js/<path:filename>')
-def send_js(filename):
-    return app.send_from_directory('js', filename)
-
-
-@app.route('/css/<path:filename>')
-def send_css(filename):
-    return app.send_from_directory('css', filename)
-
-
-@app.route('/img/<path:filename>')
-def send_img(filename):
-    return app.send_from_directory('img', filename)
-
-
-@app.route('/fonts/<path:filename>')
-def send_fonts(filename):
-    return app.send_from_directory('fonts', filename)
